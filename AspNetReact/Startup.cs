8176f5016e58ceebbdf7d2b020ea0d5634bb3885
Reinier Suarez Blanco <reiniersb1125@gmail.com>
2019-04-21 21:32:31 -0400

    Add project files.

---
+++ AspNetReact/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetReact.Startup))]
+namespace AspNetReact
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
